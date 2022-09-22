9ec56ec9f080bf66bc21d44c195d050aacb88160
FriedCheese2006 <49689423+FriedCheese2006@users.noreply.github.com>
2022-09-22 15:42:22 -0400

    Fixed human error.

--- DeviceDrivers/ContactSensorPlus_Driver.groovy
+++ DeviceDrivers/ContactSensorPlus_Driver.groovy
@@ -1,14 +1,14 @@
 /**
  *
- * Sensor Groups+_Virtual CO Detector
+ * Sensor Groups+_Virtual Contact Sensor
  *
  * Copyright 2022 Ryan Elliott
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
  * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
  * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
@@ -18,27 +18,27 @@
  */
 
 metadata {
-    definition(
-            name: "Sensor Groups+_Virtual CO Detector",
-            namespace: "rle.sg+",
-            author: "Ryan Elliott")
-            {
-                capability "Carbon Monoxide Detector"
-                capability "Actuator"
-
-                attribute "carbonMonoxide", "enum", ["clear", "detected"]
-                attribute "TotalCount", "number"
-                attribute "TotalDetected", "number"
-                attribute "TotalClear", "number"
-                attribute "CODetectedList", "string"
-                attribute "CODetectedThreshold", "number"
-            }
+	definition(
+    name: "Sensor Groups+_Virtual Contact Sensor",
+    namespace: "rle.sg+",
+    author: "Ryan Elliott") 
+		{
+		capability "ContactSensor"
+		capability "Actuator"
+			
+		attribute "contact", "enum", ["closed", "open"]
+		attribute "TotalCount", "number"
+		attribute "TotalOpen", "number"
+		attribute "TotalClosed", "number"
+		attribute "OpenList", "string"
+		attribute "OpenThreshold", "number"
+	}
 }
 
 def installed() {
-    log.warn "installed..."
+	log.warn "installed..."
 }
 
 def updated() {
-    log.info "updated..."
+	log.info "updated..."
 }
